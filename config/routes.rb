1cf724ece1a3c632676e2f72fdd1d256d3bd342b
AriSendic <rabbisendic@gmail.com>
2016-07-19 19:56:51 -0500

    added authentication and customized toolbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  get '/' => 'products#index'
   get '/products' => 'products#index'
-  
   get '/products/new' => 'products#new' 
   #  has to be before wildcard
   post '/products' => 'products#create'
@@ -10,6 +10,12 @@ Rails.application.routes.draw do
   get '/products/:id/edit' => 'products#edit'
   patch '/products/:id' => 'products#update'
   delete '/products/:id' => 'products#destroy'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+ 
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
