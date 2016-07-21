f1f55dd883c9a3b792ed49e82869420a9eef04c8
AriSendic <rabbisendic@gmail.com>
2016-07-21 16:31:22 -0500

    Fixed bug in price edit, removed image from edit, fixed indents

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,21 @@
 class OrdersController < ApplicationController
- def create
-  @product=Product.find_by(id: params[:product_id])
-  total_price = @product.price * params[:quantity].to_i
-  total_tax = @product.tax * params[:quantity].to_i
-  order = Order.new(
-    user_id: current_user.id,
-    quantity: params[:quantity],
-    product_id: params[:product_id],
-    subtotal: total_price,
-    tax: total_tax,
-    total: total_price + total_tax
+  def create
+    product = Product.find_by(id: params[:product_id])
+    total_price = product.price * params[:quantity].to_i
+    total_tax = product.tax * params[:quantity].to_i
+    order = Order.new(
+      user_id: current_user.id,
+      quantity: params[:quantity],
+      product_id: params[:product_id],
+      subtotal: total_price,
+      tax: total_tax,
+      total: total_price + total_tax
     ) 
-  order.save
-  redirect_to "/orders/#{order.id}"
- end
- def show
-   @order = Order.find_by(id: params['id'])
-
- end
+    order.save
+    redirect_to "/orders/#{order.id}"
+  end
+   
+  def show
+    @order = Order.find_by(id: params['id'])
+  end
 end
